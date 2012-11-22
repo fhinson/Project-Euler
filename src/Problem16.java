277ff18627b41f3c853d7fc5d24ec54eec0233ea
Francis Hinson <francis@orchive.com>
2012-11-22 17:02:46 -0500

    16

--- src/Problem16.java
+++ src/Problem16.java
@@ -1,37 +1,31 @@
+import java.math.BigInteger;
+import java.util.ArrayList;
+
 
 public class Problem16 {
 
 	public static void main(String[] args) {
-		int listed[] = lister(convert((long) Math.pow(2, 1000)));
-		long sum = 0;
-		for (int i = 0; i<listed.length;i++){
-			sum+=listed[i];
-			System.out.println(listed[i] + " : " + sum);
+		BigInteger value= new BigInteger("10715086071862673209484250490600018105614048117055336074437503883703510511249361224931983788156958581275946729175531468251871452856923140435984577574698574803934567774824230985421074605062371141877954182153046474983581941267398767559165543946077062914571196477686542167660429831652624386837205668069376");
+		
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		BigInteger ten = new BigInteger("10");
+		while (!value.equals(BigInteger.ZERO))
+		{
+		    list.add(0, value.mod(ten).intValue());
+		    value = value.divide(ten);
 		}
+		
+		int sum = 0;
+		
+		for (int i = 0; i < list.size(); i++){
+			sum+=list.get(i);
+		}
+		
 		System.out.println("The sum is " + sum);
+		
 	}
 	
-	public static String convert (long x) {
-		return Long.toString(x);
-	}
 
-	public static int[] lister(String x){
-		long z = Long.parseLong(x);
-		int y = x.length() - 1;
-		
-		int [] array = new int[y+1];
-		
-		for (int counter = y; counter >= 0; counter--) {
-			
-			double a = z / (Math.pow(10, counter));
-			
-			int b = (int)(Math.ceil(a) - 1);
-			if (counter == 0){
-				b = b+1;
-			}
-			array[y-counter]=b;
-			z = (int)(z - (b*Math.pow(10, counter))); 
-		}	
-		return array;
-	}
+//consulted http://stackoverflow.com/questions/13206556/split-biginteger-into-digits-and-put-them-in-an-array
+	
 }
